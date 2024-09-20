19f2a34941d5b5186f34a8b14248d4201370f02f
dwifmeister <dwifmeister@gmail.com>
2024-09-20 12:05:03 +0200

    dice project Alex Sander, og Tobias

--- src/opgave02/Craps.java
+++ src/opgave02/Craps.java
@@ -10,6 +10,7 @@ public class Craps {
     private static int timesWon = 0;
     private static int timesLost = 0;
     private static int eyes = 0;
+    private static int totalRolls = 0;
 
 
     public static void main(String[] args) {
@@ -31,40 +32,30 @@ public class Craps {
 
     private static void playCraps() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("press [enter] to play: ");
+        System.out.print("Press [enter] to play: ");
         String answer = scanner.nextLine();
+        boolean winCon = true;
 
         while (!answer.equals("stop")) {
             int[] faces = rollDice();
             eyes = faces[0] + faces[1];
-            System.out.println("You rolled: " + eyes);
+            System.out.println("\nYou rolled: " + eyes);
             System.out.println();
             updateStatistics(faces);
 
             if (rollCount == 1 && eyes == 7 || rollCount == 1 && eyes == 11) {
-                endText(true);
+                endText(winCon);
                 answer = scanner.nextLine();
             } else if (rollCount == 1 && eyes == 2 || rollCount == 1 && eyes == 3 || rollCount == 1 && eyes == 12) {
-                endText(false);
+                endText(!winCon);
                 answer = scanner.nextLine();
-
             } else if (rollCount == 1) {
                 point = eyes;
                 System.out.println(point + " Is your point.");
-                answer = scanner.nextLine();
+                System.out.printf("%10s", "-------------------------");
             }
 
-            if (eyes != point) {
-                System.out.println(eyes + " is not your point.");
-                System.out.println("Press [enter] to roll again");
-                answer = scanner.nextLine();
-            }
-            if (rollCount > 1 && eyes == 7)
-                endText(false);
-
-            if (eyes == point && rollCount > 1) {
-                endText(true);
-            }
+            rollForPoint(point);
             answer = scanner.nextLine();
         }
         printStatistics();
@@ -77,31 +68,47 @@ public class Craps {
     }
 
 
-    private static void endText(boolean string) {
-        if (true) {
-            System.out.println("\nWinner winner, chicken dinner");
-            timesWon++;
-            System.out.println("Press [enter] to play again, or type stop and press [enter] to stop");
+    private static boolean rollForPoint(int point){
+        boolean winCon = true;
+        if (rollCount > 1 && eyes != point && eyes != 7) {
+            System.out.println(eyes + " is not your point.");
+            System.out.printf("%10s", "-------------------------");
+            System.out.println("\nPress [enter] to roll again");
+        }
+        if (rollCount > 1 && eyes == 7)
+            endText(!winCon);
 
-        } else if (false)
-            System.out.println("\nSorry you lost");
+        if (rollCount > 1 && eyes == point) {
+            endText(winCon);
+        }
+        return true;
+    }
+    private static void endText(boolean winCon) {
+        if (winCon) {
+            System.out.println("\nWinner winner, chicken dinner");
+            System.out.println();
+            timesWon++;
+        } else if (!winCon)
+            System.out.println("\nSorry, you lost!");
+        System.out.println();
         timesLost++;
-        System.out.println("Press [enter] to play again, or type stop and press [enter] to stop");
+        System.out.print("Press [enter] to play again, or type stop and press [enter] to stop: ");
+        rollCount = 0;
+        point = 0;
     }
 
 
     private static void updateStatistics(int[] temp) {
         rollCount++;
+        totalRolls++;
     }
 
 
     private static void printStatistics() {
-        System.out.println("\nResults.");
-        System.out.printf("%17s", "---------------");
+        System.out.println("\n-------- Results.--------");
         System.out.println("\nYou have won " + timesWon + " times");
         System.out.println("\nYou have lost " + timesLost + " times");
-        System.out.println("\nyou rolled: " + rollCount + " times.");
-        System.out.printf("%17s", "---------------");
-        System.out.println("\nPress [enter] to play again.");
+        System.out.println("\nyou rolled: " + totalRolls + " times.");
+        System.out.printf("-------------------------");
     }
 }
